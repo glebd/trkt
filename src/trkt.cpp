d876ae054d4578edef4bc720b0d9b1d8acd29086
Gleb Dolgich <glebd70@gmail.com>
2022-02-06 21:01:21 +0000

    Add empty console menu

--- src/trkt.cpp
+++ src/trkt.cpp
@@ -1,12 +1,23 @@
 #include <fmt/format.h>
+#include <cli/cli.h>
+#include <cli/clifilesession.h>
+#include <cli/filehistorystorage.h>
 #include <iostream>
+#include <memory>
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    std::cout << fmt::format("Hello world!\n");
-#if __cplusplus >= 202002L
-    // C++20 (and later) code
-    std::cout << "C++20\n";
-#endif
-    return 0;
+    try {
+        cli::CmdHandler colorCmd;
+        auto rootMenu = std::make_unique<cli::Menu>("trkt");
+        cli::Cli cli( std::move(rootMenu) );
+        cli.ExitAction( [](auto& out){ out << "Bye.\n"; } );
+        cli::CliFileSession input(cli);
+        input.Start();
+        return 0;
+    }
+    catch(...) {
+        std::cerr << "Unknown exception\n";
+    }
+    return -1;
 }
