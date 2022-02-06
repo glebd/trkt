4c3c40e56458d821bb46db3ff4f45fdbb7c538d4
Gleb Dolgich <glebd70@gmail.com>
2022-02-06 22:53:19 +0000

    Menu: handle errors better

--- src/trkt.cpp
+++ src/trkt.cpp
@@ -12,12 +12,22 @@ int main(int /*argc*/, char** /*argv*/)
         auto rootMenu = std::make_unique<cli::Menu>("trkt");
         cli::Cli cli( std::move(rootMenu) );
         cli.ExitAction( [](auto& out){ out << "Bye.\n"; } );
+        cli.StdExceptionHandler(
+                [](std::ostream& os, const std::string& cmd, const std::exception& e)
+                {
+                    os << fmt::format("Exception '{}' caught handling command '{}'\n", e.what(), cmd);
+                }
+        );
         cli::CliFileSession input(cli);
         input.Start();
-        return 0;
+    }
+    catch(const std::exception& e) {
+        std::cerr << fmt::format("Exception: {}\n", e.what());
+        return -1;
     }
     catch(...) {
         std::cerr << "Unknown exception\n";
+        return -1;
     }
-    return -1;
+    return 0;
 }
